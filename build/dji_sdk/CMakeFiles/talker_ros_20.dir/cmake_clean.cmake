9d71202999b257e72892817652a3c336d0cee2ce
yijh0611 <yijh0611@naver.com>
2022-10-25 14:37:37 +0900

    20221025_1437

---
+++ build/dji_sdk/CMakeFiles/talker_ros_20.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/aims/catkin_ws/devel/.private/dji_sdk/lib/dji_sdk/talker_ros_20"
+  "/home/aims/catkin_ws/devel/.private/dji_sdk/lib/dji_sdk/talker_ros_20.pdb"
+  "CMakeFiles/talker_ros_20.dir/src/talker_ros_20.cpp.o"
+  "CMakeFiles/talker_ros_20.dir/src/talker_ros_20.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker_ros_20.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/dji_sdk/CMakeFiles/talker_ros_22.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/aims/catkin_ws/devel/.private/dji_sdk/lib/dji_sdk/talker_ros_22"
+  "/home/aims/catkin_ws/devel/.private/dji_sdk/lib/dji_sdk/talker_ros_22.pdb"
+  "CMakeFiles/talker_ros_22.dir/src/talker_ros_22.cpp.o"
+  "CMakeFiles/talker_ros_22.dir/src/talker_ros_22.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker_ros_22.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
